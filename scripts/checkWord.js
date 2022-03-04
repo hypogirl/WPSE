4c9d72bc8c9460f3f98d2123a46a65112ad66b48
joão <74837451+slocknad@users.noreply.github.com>
2022-03-04 15:11:36 +0000

    working

--- scripts/checkWord.js
+++ scripts/checkWord.js
@@ -1,26 +1,29 @@
 function checkWord(guess, answer) {
-    visited = new Object();
-    squares = new Array();
+    let visited = new Object();
+    let squares = new Array();
 
     for (let i = 0; i < answer.length; i++) {
         let count = 0;
         for (const l of answer) if (l == guess[i]) count++;
         if (guess[i] == answer[i]) {
-            count--;
-            visited[guess[i]] = count;
             squares[i] = green;
+            if (!(guess[i] in visited)) {
+                count--;
+                visited[guess[i]] = count;
+            }
+            else visited[guess[i]] = visited[guess[i]] - 1;
         }
         else {
-            visited[guess[i]] = count;
             squares[i] = black;
+            if (!(guess[i] in visited)) visited[guess[i]] = count;
         }
     }
 
     for (let i = 0; i < answer.length; i++) {
         if (guess[i] == answer[i]) continue;
         if (answer.includes(guess[i]) && visited[guess[i]]) {
-            squaresWord[i] = yellow;
-            visited[guess[i]]--;
+            squares[i] = yellow;
+            visited[guess[i]] = visited[guess[i]] - 1;
         }
     }
 
