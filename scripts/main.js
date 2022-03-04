2f8d7705fac47c539f7b6fd4be44a7599e36f68d
joão <74837451+slocknad@users.noreply.github.com>
2022-03-04 15:32:32 +0000

    functional guess building

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
-var letters = 5; var tries = 6;
+var letters = 5, tries = 6, guess = "", answer = "HELLO";
 
 function addLetter(letter) {
     let letterSquare = document.getElementsByClassName("row")[6-tries].getElementsByClassName("col")[5-letters];
     letterSquare.innerHTML = letter;
+    guess += letter;
     letters--;
 }
 
@@ -10,28 +11,38 @@ function removeLetter() {
     if (letters < 5) letters++;
     let letterSquare = document.getElementsByClassName("row")[6-tries].getElementsByClassName("col")[5-letters];
     letterSquare.innerHTML = "";
+    guess = guess.slice(0, -1);
+}
+
+function updateColours(squares) {
+    for (let i = 0; i < 5; i++) {
+        square = document.getElementsByClassName("row")[6-tries].getElementsByClassName("col")[i];
+        square.style.backgroundColor = squares[i];
+    }
 }
 
 document.onkeydown = function() {
     var key = event.keyCode || event.charCode;
     //backspace := 8
     console.log(key);
-    if (key != 8 && (key < 65 || key > 90)) return;
+    if (key != 8 && key != 13 && (key < 65 || key > 90)) return;
     let letter = String.fromCharCode(key);
-
+    
     if (letters) {
+        if (key == 13) return;
         if (key == 8) removeLetter();
         else addLetter(letter);
     }
-    else if (tries) {
-        if (key == 8) return
-        //squares = checkWord(guess, answer);
-        //if (!squares.includes(False) && !squares.includes(null)) victory();
+    else if (key == 8) removeLetter();
+    else if (key == 13 && tries) {
+        console.log(guess,answer);
+        squares = checkWord(guess, answer);
+        guess = "";
+        //if (!squares.includes(yellow) && !squares.includes(black)) victory();
         //else {
-            //updateColours(squares);
+            updateColours(squares);
             tries--;
             letters = 5;
-            addLetter(letter);
         //}
     }
 };
