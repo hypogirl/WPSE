64b32200b8ab704f0a14a1115c8aae426f260ead
joão <74837451+slocknad@users.noreply.github.com>
2022-03-04 13:53:11 +0000

    working letter input

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 var letters = 5; var tries = 6;
 
+function addLetter(letter) {
+    let letterSquare = document.getElementsByClassName("row")[6-tries].getElementsByClassName("col")[5-letters];
+    letterSquare.innerHTML = letter;
+    letters--;
+}
+
+function removeLetter() {}
+
 document.onkeydown = function() {
     var key = event.keyCode || event.charCode;
     //backspace := 8
@@ -14,18 +22,16 @@ document.onkeydown = function() {
         }
         else {
             addLetter(letter);
-            letters--;
         }
     }
     else if (tries) {
-        squares = checkWord(guess, answer);
-        if (!squares.includes(False) && !squares.includes(null)) victory();
-        else {
-            updateColours(squares);
+        //squares = checkWord(guess, answer);
+        //if (!squares.includes(False) && !squares.includes(null)) victory();
+        //else {
+            //updateColours(squares);
             tries--;
             letters = 5;
             addLetter(letter);
-            letters--;
-        }
+        //}
     }
 };
