2a0251e0e227a38fac9e96393a97def69a3cf2e2
joão <74837451+slocknad@users.noreply.github.com>
2022-03-04 14:02:43 +0000

    working adding/deleting letters

--- scripts/main.js
+++ scripts/main.js
@@ -6,25 +6,25 @@ function addLetter(letter) {
     letters--;
 }
 
-function removeLetter() {}
+function removeLetter() {
+    if (letters < 5) letters++;
+    let letterSquare = document.getElementsByClassName("row")[6-tries].getElementsByClassName("col")[5-letters];
+    letterSquare.innerHTML = "";
+}
 
 document.onkeydown = function() {
     var key = event.keyCode || event.charCode;
     //backspace := 8
-
-    if (key != 8 && key < 65 && key > 90) return
+    console.log(key);
+    if (key != 8 && (key < 65 || key > 90)) return;
     let letter = String.fromCharCode(key);
 
     if (letters) {
-        if (key == 8) {
-            removeLetter();
-            letters++;
-        }
-        else {
-            addLetter(letter);
-        }
+        if (key == 8) removeLetter();
+        else addLetter(letter);
     }
     else if (tries) {
+        if (key == 8) return
         //squares = checkWord(guess, answer);
         //if (!squares.includes(False) && !squares.includes(null)) victory();
         //else {
