7b3a871b1a8ad8c939221d7642983d31338d996f
joão <74837451+slocknad@users.noreply.github.com>
2022-03-05 23:12:33 +0000

    better implementation

--- scripts/setUp.js
+++ scripts/setUp.js
@@ -3,12 +3,11 @@ const yellow = "#878119";
 const black = "#495057";
 const errorLength = document.getElementById("error-length");
 const errorContent = document.getElementById("error-content");
+const uiKeys = document.getElementsByClassName("key");
 var letters = 5;
 var guess = new String();
+var keyboard = new Object();
+var tries = 6;
 //var vars = getCookieVars();
-const answer = words[getAnswerIndex()];
-var tries;
-//if (currentDate == vars[0]) tries = vars[0];
-//else
-tries = 6;
+//var tries = vars[0];
 //var stats = vars[1];
