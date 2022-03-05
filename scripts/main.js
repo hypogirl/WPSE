7be2031d00029579460e171526c5ef2d510447ed
joão <74837451+slocknad@users.noreply.github.com>
2022-03-05 17:53:46 +0000

    error messages

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@ function addLetter(letter) {
     letterSquare.innerHTML = letter;
     guess += letter;
     letters--;
+    letterSquare.classList.remove("border-secondary");
+    letterSquare.classList.add("border-primary");
+    letterSquare = document.getElementById('rows').getElementsByClassName("row")[6-tries].getElementsByClassName("col")[5-letters];
 }
 
 function removeLetter() {
@@ -10,35 +13,63 @@ function removeLetter() {
     let letterSquare = document.getElementById('rows').getElementsByClassName("row")[6-tries].getElementsByClassName("col")[5-letters];
     letterSquare.innerHTML = "⠀";
     guess = guess.slice(0, -1);
+    letterSquare.classList.remove("border-primary");
+    letterSquare.classList.add("border-secondary");
 }
 
 function updateColours(squares) {
     for (let i = 0; i < 5; i++) {
         square = document.getElementById('rows').getElementsByClassName("row")[6-tries].getElementsByClassName("col")[i];
         square.style.backgroundColor = squares[i];
+        square.classList.remove("border-primary");
+        square.classList.add("border-invisible");
     }
     tries--;
     letters = 5;
 }
 
-document.onkeydown = function() {
+document.onkeydown = async () => {
     var key = event.keyCode || event.charCode;
-    if (key != 8 && key != 13 && (key < 65 || key > 90)) return;
+    if (key != 8 && key != 13 && (key < 65 || key > 90))
+        return;
     let letter = String.fromCharCode(key);
-    
+
     if (letters) {
-        if (key == 13) return;
-        if (key == 8) removeLetter();
-        else addLetter(letter);
+        if (key == 13) {
+            errorLength.classList.remove("display-none");
+            errorLength.classList.add("display");
+            await sleep(2000);
+            errorLength.classList.add("display-none");
+            errorLength.classList.remove("display");
+            return;
+        };
+        if (key == 8)
+            removeLetter();
+        else
+            addLetter(letter);
     }
-    else if (key == 8) removeLetter();
+    else if (key == 8) {
+        removeLetter();
+    }
     else if (key == 13 && tries) {
-        console.log(guess,answer);
+        if (!words.includes(guess)) {
+            errorContent.classList.remove("display-none");
+            errorContent.classList.add("display");
+            await sleep(2000);
+            errorContent.classList.add("display-none");
+            errorContent.classList.remove("display");
+            return;
+        }
+        console.log(guess, answer);
         squares = checkWord(guess, answer);
         guess = "";
         //if (!squares.includes(yellow) && !squares.includes(black)) victory();
         //else {
-            updateColours(squares);
+        updateColours(squares);
         //}
     }
 };
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- scripts/setUp.js
+++ scripts/setUp.js
@@ -1,10 +1,12 @@
-var green = "#198754";
-var yellow = "#878119";
-var black = "#495057";
+const green = "#198754";
+const yellow = "#878119";
+const black = "#495057";
+const errorLength = document.getElementById("error-length");
+const errorContent = document.getElementById("error-content");
 var letters = 5;
 var guess = new String();
 //var vars = getCookieVars();
-var answer = getAnswer();
+const answer = getAnswer();
 var tries;
 //if (currentDate == vars[0]) tries = vars[0];
 //else
