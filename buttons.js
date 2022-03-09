a8282a9abfe9f5399396974a6386e8f39b161a1a
joão <74837451+slocknad@users.noreply.github.com>
2022-03-09 10:57:39 +0000

    Create buttons.js

---
+++ buttons.js
@@ -0,0 +1,7 @@
+function settings() {
+    settingsDiv = document.getElementById("settings");
+    settingsDiv.classList.remove("display-none");
+    settingsDiv.classList.add("display");
+    settingsDiv.classList.remove("display-small");
+    settingsDiv.classList.add("display-big");
+}
