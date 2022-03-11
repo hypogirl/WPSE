d42ebb2320556cb280ea11760b25d593c4cf5df8
joão <74837451+slocknad@users.noreply.github.com>
2022-03-11 22:35:19 +0000

    themes implemented

--- scripts/setUp.js
+++ scripts/setUp.js
@@ -8,13 +8,33 @@ var guess = new String();
 var keyboard = new Object();
 var tries = 6;
 var endingStr = "Wiccle Phase Springs Eternal ";
+var green = "correct";
+var yellow = "wrongplace";
+var black = "wrong";
 //var vars = await getCookieVars();
 //var tries = vars[0];
 //var stats = vars[1];
 //var theme = vars[2];
-var theme = "2";
-var green = "correct-theme" + theme;
-var yellow = "wrongplace-theme" + theme;
-var black = "wrong-theme" + theme;
-const body = document.getElementsByTagName("body")[0];
-body.style.backgroundImage = "url(imgs/" + theme + ".jpg)";
+var theme = 1;
+updateTheme(theme);
+
+
+function updateTheme(theme) {
+    const root = document.querySelector(':root');
+    if (theme == 1) {
+        root.style.setProperty('--green', '#3a753f');
+        root.style.setProperty('--yellow', '#75653a');
+        root.style.setProperty('--black', '#343434');
+        root.style.setProperty('--background', '#181818');
+        root.style.setProperty('--text', 'black');
+        root.style.setProperty('--body', 'url(imgs/1.jpg)');
+    }
+    else {
+        root.style.setProperty('--green', '#43945d');
+        root.style.setProperty('--yellow', '#69308a');
+        root.style.setProperty('--black', '#4a4a4a');
+        root.style.setProperty('--background', '#3a546a');
+        root.style.setProperty('--text', 'white');
+        root.style.setProperty('--body', 'url(imgs/2.jpg)');
+    }
+}
